8c975d21631b29b8c80311ef7e15cf9932be8574
KombinatorVit <slonik0775@gmail.com>
2023-04-21 20:12:27 +0300

    update project

--- index.js
+++ index.js
@@ -1,19 +1,86 @@
-let getAverage = (month1, month2, month3) => {
-    let averagePerMonth = (month1 + month2 + month3) / 3;
-    return averagePerMonth;
+function power(pow) {
+    return function (num) {
+        return num ** pow
+    }
 }
-console.log(getAverage(35467, 29842, 38501));//dept1q1
-console.log(getAverage(50301, 21984, 19207));//dept1q2
-console.log(getAverage(70533, 50121, 33899))// dept2q1
-console.log(getAverage(72381, 41562, 29465))//dept2q2
 
-function printBonus(dept1AverSales, dept2AverSales) {
-    let winner// = dept1AverSales > dept2AverSales;
-    if (dept1AverSales > dept2AverSales) {
-        winner = ((dept1AverSales - dept2AverSales) / dept2AverSales * 100);
-        console.log(`По результатам первого квартала,первый отдел заработал больше на ${winner}%`)
-    } else
-        winner = ((dept2AverSales - dept1AverSales) / dept1AverSales * 100);
-    console.log(`По результатам первого квартала,второй отдел заработал больше на ${winner}%`)
 
+const power2 = (pow) => (num) => num ** pow
+
+const prices = [[100, 200], [120, 100], [200, 350]]
+
+// const deltaPriceChange = (arr) => {
+//     const sumElementArray = arr.map(el => el.reduce((acc, item) => {
+//         return item -= acc
+//     }, 0)).filter(item => item > 0)
+//     console.log(sumElementArray)
+//
+//     return sumElementArray
+//
+// }
+//
+// deltaPriceChange(prices)
+
+// const calculatePriceDelta = (arr) => {
+//     const resultArray = []
+//
+//     for (let itemArray of arr){
+//
+//         if(itemArray[1]-itemArray[0] > 0){
+//             resultArray.push(itemArray[1]-itemArray[0])
+//         }
+//
+//     }
+//     console.log(resultArray)
+//     return resultArray
+//
+// }
+//
+// calculatePriceDelta(prices)
+const calculatePriceDeltaM = (arr) => {
+
+    return arr.map(el => el[1] - el[0]).filter(el2 => el2 > 0)
 }
+
+
+console.log(calculatePriceDeltaM(prices))
+
+const arr = [2, 4, 4, 10]
+
+console.log(arr.reduce((acc, item, i) => {
+    if (i !== arr.length - 1) {
+        return acc += item
+    } else {
+        return (acc += item) / arr.length
+    }
+
+
+}, 0))
+
+
+function some(arr2, item) {
+
+    return arr2.find(el => {
+        return el === item
+    })
+}
+
+
+console.log(some(arr, 4))
+const newArr = []
+
+for (let i = 0; i < prices.length; i++) {
+    for (let j = 0; j < prices[i].length; j++) {
+        newArr.push(prices[i][j])
+    }
+}
+
+
+const arr2 = new Array(5).fill(1)
+
+console.log(arr2)
+
+
+const arr3 = Array.from({length:5}, ()=> 5)
+
+console.log(arr3)
---
+++ typescript/app.ts
@@ -0,0 +1,34 @@
+
+
+const enum Status {
+    published,
+    draft,
+    deleted
+}
+
+type ReqType = {
+    topicId: number
+    status: Status.published
+}
+type ResponseMyType = {
+    question: string
+    answer: string
+    tags: string[]
+    likes: number
+    status: Status.published
+}
+
+async function getFaqs(req: ReqType): Promise<ResponseMyType[]> {
+    const res = await fetch("/faqs", {
+        method: "POST",
+        body: JSON.stringify(req)
+    });
+    const data = await res.json();
+    return data;
+}
+
+
+
+
+
+
